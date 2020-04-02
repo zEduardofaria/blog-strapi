c9b2cfa6461c704be44590cab3eda5dc25572f23
Eduardo Faria <z.eduardofaria@gmail.com>
2020-04-02 11:46:14 -0300

    Basic blog finished

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,20 +1,41 @@
 import React from "react"
-import { Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import ArticlesComponent from "../components/articles"
+
+import "../assets/css/main.css"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <StaticQuery
+      query={graphql`
+        query {
+          allStrapiArticle {
+            edges {
+              node {
+                strapiId
+                title
+                category {
+                  name
+                }
+                image {
+                  publicURL
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div className="uk-section">
+          <div className="uk-container uk-container-large">
+            <h1>Strapi blog</h1>
+            <ArticlesComponent articles={data.allStrapiArticle.edges} />
+          </div>
+        </div>
+      )}
+    />
   </Layout>
 )
 
